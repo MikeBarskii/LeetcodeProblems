35d713bbf7a3860c3ec0cb2c91f6cf02756b541c
Mikhail <mikebarskiy@gmail.com>
2020-04-03 20:48:35 +0300

    [Introduction to Data Structure] Linked List

--- src/com/github/barskiy/Main.java
+++ src/com/github/barskiy/Main.java
@@ -1,16 +1,19 @@
 package com.github.barskiy;
 
-import com.github.barskiy.linkedlist.singlylinkedlist.MyLinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList linkedList = new MyLinkedList(); // Initialize empty LinkedList
-        linkedList.addAtHead(1);
-        linkedList.addAtTail(3);
-        linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
-        System.out.println(linkedList.get(1));
-        linkedList.deleteAtIndex(1);  // now the linked list is 1->3
-        System.out.println(linkedList.get(1));
+//        MyLinkedList linkedList = new MyLinkedList(); // Initialize empty LinkedList
+//        linkedList.addAtHead(1);
+//        linkedList.addAtTail(3);
+//        linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
+//        System.out.println(linkedList.get(1));
+//        linkedList.deleteAtIndex(1);  // now the linked list is 1->3
+//        System.out.println(linkedList.get(1));
+
+        int[] arr = {4, 4,1};
+        int number = 0;
+        for (int i : arr) number = number ^ i;
+        System.out.println(number);
     }
 }
