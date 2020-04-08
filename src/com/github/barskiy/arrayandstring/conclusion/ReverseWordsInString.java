7406e2da2809fbee52da4f9be38db75ca705dd60
Mikhail <mikebarskiy@gmail.com>
2020-04-08 13:00:12 +0300

    [Introduction to Data Structure] Array and String were finished

---
+++ src/com/github/barskiy/arrayandstring/conclusion/ReverseWordsInString.java
@@ -0,0 +1,14 @@
+package com.github.barskiy.arrayandstring.conclusion;
+
+public class ReverseWordsInString {
+
+    public String reverseWords(String s) {
+        StringBuilder builder = new StringBuilder();
+        String[] strings = s.trim().split(" +");
+        for (int i = strings.length - 1; i > 0; i--) {
+            builder.append(strings[i]).append(" ");
+        }
+
+        return builder.append(strings[0]).toString();
+    }
+}
