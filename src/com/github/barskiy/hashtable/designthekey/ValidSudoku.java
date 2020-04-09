71870598e27df073d2232675c077ba7398796966
Mikhail <mikebarskiy@gmail.com>
2020-04-09 21:13:40 +0300

    [Introduction to Data Structure] Hashtable. Design the key part is in progress

---
+++ src/com/github/barskiy/hashtable/designthekey/ValidSudoku.java
@@ -0,0 +1,37 @@
+package com.github.barskiy.hashtable.designthekey;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, List<int[]>> valueToLocation = new HashMap<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+
+                int value = board[i][j] - '0';
+                if (valueToLocation.containsKey(value)) {
+                    List<int[]> indices = valueToLocation.get(value);
+                    for (int[] index : indices) {
+                        int row = index[0];
+                        int column = index[1];
+                        if (row == i || column == j || (row / 3 == i / 3 && column / 3 == j / 3))
+                            return false;
+                    }
+                    indices.add(new int[]{i, j});
+                } else {
+                    List<int[]> index = new ArrayList<>(9);
+                    index.add(new int[]{i, j});
+                    valueToLocation.put(value, index);
+                }
+            }
+        }
+        return true;
+    }
+}
