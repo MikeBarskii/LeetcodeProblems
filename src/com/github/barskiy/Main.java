6ce38dff4756d328d345dad945fd32de3ec1bb75
Mikhail <mikebarskiy@gmail.com>
2020-04-09 20:07:05 +0300

    [Introduction to Data Structure] Hashtable. HashSet and HashMap parts were studied

--- src/com/github/barskiy/Main.java
+++ src/com/github/barskiy/Main.java
@@ -4,5 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello LeetCode");
+
+
+        System.out.println(new Main().backspaceCompare(
+                "hd#dp#czsp#####",
+                "hd#dp#cs#zspj######"));
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+        int p1 = S.length();
+        int p2 = T.length();
+        while (p1 != 0 || p2 != 0) {
+            p1 = getIndexOfNonDeletedChar(S, p1 - 1);
+            p2 = getIndexOfNonDeletedChar(T, p2 - 1);
+
+            if (p1 == -1 && p2 == -1)
+                return true;
+
+            if (p1 == -1 || p2 == -1)
+                return false;
+
+            if (S.charAt(p1) != T.charAt(p2))
+                return false;
+        }
+
+        return true;
+    }
+
+    private int getIndexOfNonDeletedChar(String s, int pointer) {
+        int charsForDeletion = 0;
+        while (pointer >= 0 && (s.charAt(pointer) == '#' || charsForDeletion != 0)) {
+            if (s.charAt(pointer) == '#')
+                charsForDeletion++;
+            else
+                charsForDeletion--;
+            pointer--;
+        }
+
+        return (pointer >= 0 ? pointer : -1);
     }
 }
---
+++ src/com/github/barskiy/hashtable/hashset/SingleNumber.java
@@ -0,0 +1,23 @@
+package com.github.barskiy.hashtable.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        Set<Integer> values = new HashSet<>();
+        for (int num : nums) {
+            if (values.contains(num)) values.remove(num);
+            else
+                values.add(num);
+        }
+        return values.iterator().next();
+    }
+
+    public int singleNumberOneMoreSolution(int[] nums) {
+        int number = 0;
+        for (int i : nums) number = number ^ i;
+        return number;
+    }
+}
